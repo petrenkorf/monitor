63e78447bfb6810fddb4fa08df9a3c13d53ad53a
Petris <petris@impacte.com.br>
2019-11-21 17:24:10 -0300

    Correção

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,40 @@
-var gulp = require("gulp");
+var gulp    = require("gulp"),
+    phpunit = require("gulp-phpunit"),
+    plumber = require('gulp-plumber'),
+    exec    = require('child_process').exec;
 
-var files = ['*.php'];
+var files = ['app/**/*.php', "tests/**/*.php"];
 
-var printFile = function (file) {
-    console.log(file);
+var isTest = function(file) {
+    return file.indexOf("tests/") === 0;
 };
 
-gulp.task('monitor', function () {
+var runTest = function(file) {
+
+    var filename = (isTest(file))
+        ? file
+        : "tests/"+file.replace(".php", "Test.php");
+
+    var options = {
+        testClass: filename,
+        stopOnFailure: true,
+        stopOnError: true
+    };
+    
+    gulp.src(file)
+        .pipe(plumber({errorHandler: function () {}}))
+        .pipe(phpunit("./vendor/bin/phpunit", options));
+};
+
+var checkFile = function (file) {
+    var isTest = file.indexOf("Test.php");   
+    (isTest >= 0) ? runTest(file) : runTest(file);
+};
+
+var monitor = function () {
     gulp.watch(files)
-        .on('change', printFile)
-        .on('add', printFile);
-});
+        .on('change', checkFile)
+        .on('add', checkFile);
+};
 
-
+exports.monitor = monitor;
